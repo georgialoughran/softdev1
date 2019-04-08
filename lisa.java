19fa2aaa0fb1d9d39fe4518f94d3ebb0bbb5079e
georgialoughran <49395950+georgialoughran@users.noreply.github.com>
2019-04-08 11:20:29 +0100

    Add files via upload

---
+++ lisa.java
@@ -0,0 +1,22 @@
+/**
+Name: lisa.java
+Created by: Georgia Loughran
+Created on: 28/01/19
+*/
+//Lisa is 9 years old and is 133.3 centimetres tall
+//Lisa weighs 27.8 kilograms
+
+
+public class lisa{
+public static void main (String[] args) {
+
+	String name = "lisa";
+	int age = 9;
+	double height = 133.3;
+	double weight = 27.8;
+
+	System.out.println(name+ " is " +age+" years old and is "+height+" centimetres tall.");
+	System.out.println(name+" weighs "+weight+" kilograms.");
+
+					}//end main
+				}//end class
